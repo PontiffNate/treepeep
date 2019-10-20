0a15974c771126de8de057cbac41d025abfe4324
Nathan Falkenburg <31420355+PontiffNate@users.noreply.github.com>
2019-10-20 16:47:13 -0400

    Map now has popups

--- src/views/GoogleAPIWrapper.js
+++ src/views/GoogleAPIWrapper.js
@@ -1,54 +1,9 @@
-import React, {Component} from "react";
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { GoogleMap } from "react-google-maps";
 
-
-// ...
-
-export class MapContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.testLocations = {
-      trees: [{lat: 43.104185, lng: -77.580177},
-        {latitude: 35.210395, longitude: -84.848504},
-        {latitude: 30.94, longitude: -99.45},
-        {latitude: 36.18, longitude: -118.89},
-        {latitude: 49.12, longitude: -79.46},
-        {latitude: 47.5524695, longitude: -122.0425407}]
-    }
-  }
-
-  displayMarkers = () => {
-    return this.testLocations.trees.map((tree, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: tree.latitude,
-       lng: tree.longitude
-     }}
-     onClick={() => console.log("You clicked me!")} />
-    })
-  }
-    render() {
-      return (
-        <Map google={this.props.google}
-             zoom={4}
-             initialCenter={{ lat: 39.8097343, lng: -98.5556199}}
-             >
-            {this.displayMarkers()}
-  
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'}
-                   />
-                  
-  
-          <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>
-                <h1>Hello World</h1>
-              </div>
-          </InfoWindow>
-        </Map>
-      );
-    }
-  }
-
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyAA_pdK9MBIEJnMoZ_8RFArBRL_okctPrg"
-})(MapContainer)
+function Map() {
+  return (
+    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 45.4211, lng: -75.6903 }}>
+      { /* We will render our data here */ }
+    </GoogleMap>
+  );
+}
--- src/views/TreeMap.js
+++ src/views/TreeMap.js
@@ -1,15 +1,61 @@
-import React, {Component} from "react";
+import React, {useState, Component} from "react";
 import "./TreeMap.css";
-import MapContainer from "./GoogleAPIWrapper";
+import { GoogleMap, Marker, InfoWindow, withGoogleMap, withScriptjs } from "react-google-maps";
+import * as parkData from "../data/testData.json";
+
+// import Map from "./GoogleAPIWrapper";
+const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export default class TreeMap extends Component {
     render() {
         return(
             <React.Fragment>
-                <div id="map-container-google-3" class="z-depth-1-half map-container-3">
-                    <MapContainer/>
+                <div style={{ width: "100vw", height: "100vh" }}>
+                  <MapWrapped
+                    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAA_pdK9MBIEJnMoZ_8RFArBRL_okctPrg`}
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `100%` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                  />
                 </div>
             </React.Fragment>
         );
     }
 }
+
+function Map() {
+    const [selectedPark, setSelectedPark] = useState(null);
+    return (
+        <GoogleMap
+          defaultZoom={10}
+          defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
+        >
+          {parkData.features.map(park => (
+            <Marker
+              key={park.properties.PARK_ID}
+              position={{
+                lat: park.geometry.coordinates[1],
+                lng: park.geometry.coordinates[0]
+              }}
+              onClick={() => { setSelectedPark(park); }}
+              
+            />
+          ))}
+    
+          {selectedPark && (
+            <InfoWindow
+              onCloseClick={() => { setSelectedPark(null); }}
+              position={{
+                lat: selectedPark.geometry.coordinates[1],
+                lng: selectedPark.geometry.coordinates[0]
+              }}
+            >
+              <div>
+                <h2>{selectedPark.properties.NAME}</h2>
+                <p>{selectedPark.properties.DESCRIPTION}</p>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      );
+  }
