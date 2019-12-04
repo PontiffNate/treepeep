c7963e0ddabd342d786c8b16bb63d187cf9a2509
Bryce Murphy <theshmurph@gmail.com>
2019-12-03 22:52:30 -0500

    actual descriptions

--- src/views/About.js
+++ src/views/About.js
@@ -15,25 +15,44 @@ export default class About extends Component {
 
                 <Card style={{ width: '40rem' }}>
                     <Card.Body>
-                    <Card.Title>Lorem Ipsum</Card.Title>
-                    <Card.Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam laoreet, lacus scelerisque interdum blandit, purus sem aliquam metus, sed luctus ipsum diam ac erat. Quisque sit amet magna vitae nisl blandit pharetra. Suspendisse scelerisque porttitor ipsum sit amet bibendum. Nulla sit amet ullamcorper turpis. Curabitur at lectus ut diam venenatis rhoncus. Phasellus hendrerit auctor pulvinar. Morbi semper urna et convallis egestas. Fusce fringilla finibus facilisis. Nullam eu dignissim libero.
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Body>
+                        <Card.Title>
+                            Who We Are
+                        </Card.Title>
+                        <Card.Text>
+                            We are treepeep! But you knew that already. More specifically, we are Bryce, Christian, Nate, and Patrick. We started treepeep \
+                            as a school project to show off all the awesome trees we all see every day. After noticing there was no service in this space \
+                            already, we quickly got to work. After all - to us, a world without a tree sharing app is one we aren't ready to live in. So we \
+                            created treepeep, for lovers an aficionados of trees young and old, all across the globe!
+                        </Card.Text>
                     </Card.Body>
                 </Card>
 
-            <Card style={{ width: '40rem' }}>
-                    <Card.Body>
-                    <Card.Title>Lorem Ipsum</Card.Title>
-                    <Card.Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam laoreet, lacus scelerisque interdum blandit, purus sem aliquam metus, sed luctus ipsum diam ac erat. Quisque sit amet magna vitae nisl blandit pharetra. Suspendisse scelerisque porttitor ipsum sit amet bibendum. Nulla sit amet ullamcorper turpis. Curabitur at lectus ut diam venenatis rhoncus. Phasellus hendrerit auctor pulvinar. Morbi semper urna et convallis egestas. Fusce fringilla finibus facilisis. Nullam eu dignissim libero.
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Body>
+                <Card style={{ width: '40rem' }}>
+                <Card.Body>
+                        <Card.Title>
+                            What Is TreePeep?
+                        </Card.Title>
+                        <Card.Text>
+                            Our app is designed to be as easy to use as possible for you to get your favorite trees posted, and explore what other trees are \
+                            put up on the site. Here you can view your feed of all trees posted (or filter further if you want), like and/or comment on a post, \
+                            and view an aggregate map of all the geotagged trees in our database, further connecting you to users across the world! We hope you \
+                            enjoy what we have for you, and we look forward to seeing your best trees!
+                        </Card.Text>
                     </Card.Body>
                 </Card>
+
+                <Card style={{ width: '40rem' }}>
+                    <Card.Body>
+                        <Card.Title>
+                            Why TreePeep?
+                        </Card.Title>
+                        <Card.Text>
+                            If you enjoy trees, a great community, or nature in general, treepeep is for you! While we market ourselves as a tree-specific social \
+                            media site similar to your favorites, we are just as much a community of tree-lovers and appreciators of nature. After all, if there are \
+                            no trees, there is no site, and we find that such a niche role has great potential to start a healthy community. So come take root!
+                        </Card.Text>
+                    </Card.Body>
+                    </Card>
 
             </div>
 
